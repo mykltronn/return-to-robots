35216e8d95fb8bf9e399b446aded16b987c83c69
Michael Ashton <mykl.ashton@gmail.com>
2017-07-03 11:19:03 -0400

    initial commit of robots containing old robots thing

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+var data = require('./data.js');
+
+var mustacheExpress = require('mustache-express');
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index.mustache', { users: data.users });
+});
+
+app.get('/:indexNum', function (req, res) {
+  let i = parseInt(req.params.indexNum) - 1;
+  res.render('user.mustache', data.users[i]);
+});
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!')
+});
